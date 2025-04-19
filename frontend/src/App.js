47021d4d20027aaa5f659a5a70635a112f7d2075
SiaSmartHome <parsmail2000@gmail.com>
2025-04-19 07:10:10 -0400

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>SIA SmartHome Quotation System</h1>
+      <p>This is a placeholder React app.</p>
+    </div>
+  );
+}
+export default App;
